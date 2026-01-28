2d5cb10f72083532b8e9390d3cb663d94a0da578
senecalalexis-star <senecalalexis@gmail.com>
2026-01-27 18:49:23 -0800

    Update schema_setup.sql

--- sql/schema_setup.sql
+++ sql/schema_setup.sql
@@ -2,6 +2,10 @@
 -- PROJECT: Indie Market Strategy
 -- Database: PostgreSQL
 
+
+-- ==========================================
+-- 1. REFERENCE TABLES (Create these FIRST)
+-- ==========================================
 -- Tag (Official Steam Tags)
 CREATE TABLE tag (
 	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -18,24 +22,43 @@ CREATE TABLE game_id (
 	name text NOT NULL
 );
 
+-- ==========================================
+-- 2. DIMENSION TABLES (Create these SECOND)
+-- ==========================================
 -- Game data
 CREATE TABLE game_info (
 	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	game_id integer NOT NULL UNIQUE,
-	name text NOT NULL,
-	price real NOT NULL,
-	rating real NOT NULL,
-	release date NOT NULL,
-	follows integer NOT NULL,
-	reviews integer NOT NULL,
-	peak integer NOT NULL
+	game_id integer NOT NULL,
+	name text,
+	price real,
+	rating real,
+	release date,
+	follows integer,
+	reviews integer,
+	peak integer
+	CONSTRAINT fk_game_source FOREIGN KEY (game_id) 
+		REFERENCES game_id (game_id)
 );
 
 -- Game Playtime in Minutes
 CREATE TABLE play_time (
 	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	game_id integer,
-	game_name text,
+	name text,
 	avg_playtime_minutes integer,
 	median_playtime_minutes integer
+	CONSTRAINT fk_game_source FOREIGN KEY (game_id) 
+		REFERENCES game_id (game_id)
+);
+
+-- Game with their Tags
+CREATE TABLE game_tag (
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	game_id integer,
+	name text,
+	tag_id integer
+	CONSTRAINT fk_game_source FOREIGN KEY (game_id) 
+		REFERENCES game_id (game_id)
+	CONSTRAINT fk_game_source_tag FOREIGN KEY (tag_id) 
+		REFERENCES tag (tag_id)
 );
